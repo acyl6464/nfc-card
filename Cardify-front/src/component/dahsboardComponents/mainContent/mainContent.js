3027ce5abd67ee2273c504384411c14e8d9fa2c2
Rachid <rachidbentaleb2002@gmail.com>
2025-01-31 16:36:07 +0100

    Updated features

--- Cardify-front/src/component/dahsboardComponents/mainContent/mainContent.js
+++ Cardify-front/src/component/dahsboardComponents/mainContent/mainContent.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { FaSearch } from "react-icons/fa";
-import { BiBell } from "react-icons/bi";
 import profileImage from "../../../assets/images/profile.svg";
 import "./mainContent.css";
+import notificationvector from '../../../assets/images/notificationvector.svg'
+import searchinputvector from '../../../assets/images/search-iconvector.svg'
 
 const MainContent = ({ isCollapsed }) => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,19 +13,20 @@ const MainContent = ({ isCollapsed }) => {
       <div className="header-search-bar">
         {/* Search Bar */}
         <div className="search-bar">
-          <FaSearch className="search-icon" />
+          <img src={searchinputvector} alt="serch-input-vector"/>
           <input
             type="text"
             placeholder="Search..."
             className="search-input"
             onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
           />
         </div>
 
         {/* Profile & Notifications */}
         <div className="profile-section">
           <div className="notifications">
-            <BiBell className="notification-icon" />
+          <img src={notificationvector} alt="notification-vector"/>
           </div>
           <div className="profile-avatar">
             <img src={profileImage} alt="Profile" />
